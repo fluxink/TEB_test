074837e4f050650b2929ad7a9186f9fe84894fd2
fluxink <pavelvolk300@gmail.com>
2023-04-05 01:02:44 +0300

    host

--- teb_test_simple/web.py
+++ teb_test_simple/web.py
@@ -89,4 +89,4 @@ def user(user_id):
 
 if __name__ == '__main__':
     session = init_db()
-    app.run(debug=True, port=os.getenv('PORT', 80))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 80))
